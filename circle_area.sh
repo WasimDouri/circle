b3e1d02f2047a4cf8d6b654e95de25792c42cbe1
WasimDouri <31213214+WasimDouri@users.noreply.github.com>
2025-10-22 21:24:37 +0100

    Update circle_area.sh

--- circle_area.sh
+++ circle_area.sh
@@ -6,4 +6,4 @@ radius=$1
 # Using bc for floating point calculation
 #area=$(echo "scale=4; 3.1416 * $radius * $radius" | bc)
 
-echo "The area of the circle with radius $radius is $area"
+echo "The area of the circle with radius $radius is $radius"
