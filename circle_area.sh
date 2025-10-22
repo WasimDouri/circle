2acdb975d4c9e0076b35de3edcae651528ed7b8b
WasimDouri <31213214+WasimDouri@users.noreply.github.com>
2025-10-22 21:23:16 +0100

    Update circle_area.sh

--- circle_area.sh
+++ circle_area.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Check if radius is passed
-if [ -z "$1" ]; then
-  echo "Usage: $0 <radius>"
-  exit 1
-fi
+
 
 radius=$1
 # Using bc for floating point calculation
-area=$(echo "scale=4; 3.1416 * $radius * $radius" | bc)
+#area=$(echo "scale=4; 3.1416 * $radius * $radius" | bc)
 
 echo "The area of the circle with radius $radius is $area"
