7c43698b4eb0fd2e6ab11403a7b98124612c7afd
WasimDouri <31213214+WasimDouri@users.noreply.github.com>
2025-10-23 21:01:52 +0100

    Update circle_area.sh

--- circle_area.sh
+++ circle_area.sh
@@ -2,7 +2,9 @@
 
 # Access the first argument passed to the script
 echo "Received parameter: $1"
+PI=3.1415
 radius=$1
-area=3.14 * $radius * $radius
+#area=3.14 * $radius * $radius
+area=$(echo "scale=2; $PI * $radius * $radius" | bc)
 echo "$area"
 
